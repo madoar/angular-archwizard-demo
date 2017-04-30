377424da1b64bb8b75ec4bb8ba406eb642b4ed2a
Marc Arndt <marc.arndt@masimi.de>
2017-04-30 20:53:31 +0200

    Changed the demo for ng2-archwizard 1.4.0 - added a select option to change the navigation bar layout

--- src/app/pizza.component.ts
+++ src/app/pizza.component.ts
@@ -12,6 +12,7 @@ export class WizardTestComponent {
   public navBarLocation = "top";
   public navBarWidth = "none";
   public showBorder = false;
+  public navigationLayout = "small";
 
   public dough: string;
 
