788923b6cba36110d964f5852f9477c4625f6016
avrilverhaeghen <avril@verhaeghen.me>
2017-09-29 10:00:58 +0200

    Moved custom CSS, bugfix

--- src/app/custom-css/custom-css.component.ts
+++ src/app/custom-css/custom-css.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-custom-css',
   templateUrl: './custom-css.component.html',
-  styleUrls: ['./custom-css.component.css']
+  styleUrls: ['./custom-css.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CustomCssComponent implements OnInit {
 
