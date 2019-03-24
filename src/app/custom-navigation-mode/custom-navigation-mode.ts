1bb011f9842a7684937e4b3d0a317f5541eb6dfc
Evgeny Arshinov <earshinov@gmail.com>
2019-03-24 21:28:06 +0300

    In the example custom navigation mode, re-use the base implementation of SemiStrictNavigationMode.isNavigable instead of copying-and-pasting

--- src/app/custom-navigation-mode/custom-navigation-mode.ts
+++ src/app/custom-navigation-mode/custom-navigation-mode.ts
@@ -3,30 +3,29 @@ import {SemiStrictNavigationMode} from 'angular-archwizard';
 /**
  * A custom NavigationMode
  *
- * Allows to navigate backward to any step (like `StrictNavigationMode`)
+ * Allows to navigate backward to any step (like [[StrictNavigationMode]])
  * and forward to steps the user has already visited before.
  *
- * Next and Previous buttons work as in `StrictNavigationMode`.
+ * Next and Previous buttons work as in [[StrictNavigationMode]] and [[SemiStrictNavigationMode]].
  */
 export class CustomNavigationMode extends SemiStrictNavigationMode {
 
   // @override
   protected canTransitionToStep(destinationIndex: number) {
-    // Works as in strict mode
-    return this.wizardState.wizardSteps
-        .filter((step, index) => index < destinationIndex && index !== this.wizardState.currentStepIndex)
-        .every(step => step.completed || step.optional);
+
+    // Use the base implementation of `isNavigable` from `SemiStrictNavigationMode`
+    return super.isNavigable(destinationIndex);
   }
 
   // @override
   public isNavigable(destinationIndex: number): boolean {
 
-    // allow returning to previous steps
+    // Allow returning to previous steps
     if (destinationIndex < this.wizardState.currentStepIndex) {
       return true;
     }
 
-    // allow returning to any previously visited ("completed" steps)
+    // Allow returning to any previously visited ("completed" steps)
     const step = this.wizardState.wizardSteps[destinationIndex];
     if (step && step.completed) {
       return true;
