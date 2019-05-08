6ba5e77964c7e0baffbd41b9fb66b362ccdb8a06
Marc Arndt <marc.arndt@masimi.de>
2019-05-08 22:08:11 +0200

    - fix dist folder path

--- scripts/install-angular-archwizard.sh
+++ scripts/install-angular-archwizard.sh
@@ -3,7 +3,7 @@ ARCHWIZARD_DEMO_DIR=$1
 ARCHWIZARD_REPOSITORY_URL="https://github.com/madoar/angular-archwizard.git"
 ARCHWIZARD_VERSION=$(grep '"angular-archwizard":' package.json | cut -d '"' -f4 | tr -d '[[:space:]]')
 ARCHWIZARD_DIR="$ARCHWIZARD_DEMO_DIR/../angular-archwizard"
-ARCHWIZARD_DIST_DIR="$ARCHWIZARD_DEMO_DIR/dist"
+ARCHWIZARD_DIST_DIR="$ARCHWIZARD_DIR/dist"
 
 if [[ "$ARCHWIZARD_VERSION" == *develop ]]; then
   git clone $ARCHWIZARD_REPOSITORY_URL $ARCHWIZARD_DIR
