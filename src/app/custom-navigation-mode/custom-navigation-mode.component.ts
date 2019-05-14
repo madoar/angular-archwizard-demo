fba672a6906e85ed2ec9805c6dcd9bbd94fb9918
Evgeny Arshinov <earshinov@gmail.com>
2019-05-14 18:26:28 +0300

    Demonstrate how to define custom CSS styles to make optional steps in the wizard on the 'Free navigation mode' demo page look like default steps, in order to replicate the behavior of the old `FreeNavigationMode` exactly.
    Unfortunately, the process of CSS customization is far from present.

--- src/app/custom-navigation-mode/custom-navigation-mode.component.ts
+++ src/app/custom-navigation-mode/custom-navigation-mode.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {CustomNavigationMode} from './custom-navigation-mode';
-import {WizardComponent, NavigationMode} from 'angular-archwizard';
 
 @Component({
   selector: 'app-custom-navigation-mode',
