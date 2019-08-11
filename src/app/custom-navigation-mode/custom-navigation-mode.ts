8edc8094028fe77ec707268c62199fa05d967813
Evgeny Arshinov <earshinov@gmail.com>
2019-08-11 17:03:39 +0300

    Add a note on the custom-navigation-mode demo page saying that in a real application one should use navigateForward="visited"

--- src/app/custom-navigation-mode/custom-navigation-mode.ts
+++ src/app/custom-navigation-mode/custom-navigation-mode.ts
@@ -1,9 +1,10 @@
 import {WizardComponent, ConfigurableNavigationMode} from 'angular-archwizard';
 
 /**
- * A custom NavigationMode which allows to navigate backward to any step and forward to steps the user has already visited before.
+ * A custom `NavigationMode` which allows to navigate backward to any step and forward to steps which were already visited before.
  *
- * This class only exists for demonstration purposes.  In a real application, one should simple use `[navigateForward]="visited"`.
+ * This class only serves demonstration purposes.  In a real application, use
+ * `<aw-wizard [awNavigationMode] navigateForward="visited">...</aw-wizard>`.
  */
 // Extend ConfigurableNavigationMode for its implementation of canTransitionToStep
 export class CustomNavigationMode extends ConfigurableNavigationMode {
